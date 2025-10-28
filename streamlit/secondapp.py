9b1f60652090b0fa17bdc0434424dd87bb744615
amardeeplakkuntla <lakkuntlaamardeep@gmail.com>
2025-10-28 16:57:37 +0530

    this are related to my streamlit

--- streamlit/secondapp.py
+++ streamlit/secondapp.py
@@ -1,19 +1,31 @@
 import streamlit as st 
+import requests
 
+res=requests.get("https://openwhyd.org/hot/electro?format=json")
 
-cols=st.columns([0.3 , 3 , 0.3])
+if res.status_code==200:
+  res=res.json()
+  tracks=res['tracks']
+
+  cols=st.columns([0.3 , 3 , 0.3])
 
 
     
 with cols[1]:
     st.subheader("musicon")
-    with st.container(border=True):
-      cols1=st.columns([3, 2])
-      with cols1[0]:
-        title="The Twilight sad - There's A girl in the corer"
-        st.write(title[:34 ] + '.......')
-      with cols1[1]:
-        st.write("https://img.youtube.com/vi/l8H38U4nZag/hqdefault.jpg")
+    search_phrase= st.text_input("🔍 search track",placeholder=" 🔍 search track",label_visibility='collapsed')
+    for track in tracks:
+      if search_phrase in track['name']:
+       with st.container(border=True):
+        cols1=st.columns([3, 2])
+       with cols1[0]:
+         title=track['name']
+         st.write(title[:34 ] + '.......')
+         st.write(f" 💃 {track['uNm']}")
+         url = track['trackUrl']
+         st.link_button("play now",f"https:{url}",type="primary")
+       with cols1[1]:
+        st.image(track['img'])
           
         
       

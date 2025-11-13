adce0331304ad0f0d87152cb093e68f0e5ee94bd
amardeeplakkuntla <lakkuntlaamardeep@gmail.com>
2025-11-13 17:35:56 +0530

    this is payments code

---
+++ paymentss/paytm.py
@@ -0,0 +1,2 @@
+def paytm_payment(amount):
+    print(f"your payment rs {amount} successfull in paytm")
---
+++ paymentss/phonepe.py
@@ -0,0 +1,2 @@
+def phonepay_pay(amount):
+    print(f"your payment rs {amount} successfull in phonpe")

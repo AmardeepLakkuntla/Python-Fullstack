c1aa1202fdbadad3b73cfb293a13ee220c12ba92
amardeeplakkuntla <lakkuntlaamardeep@gmail.com>
2025-11-22 20:03:49 +0530

    this are sets assignments

---
+++ sets_assignments.py/assignment_2.py
@@ -0,0 +1,5 @@
+games={"badminton","cricket","chess"}
+games_2=games.discard("throwball")
+print(games)
+games_1=games.remove("throwball")
+print(games)
---
+++ sets_assignments.py/assignment_3.py
@@ -0,0 +1,4 @@
+my_games={"cricket","football","tennis"}
+friend_games={"badminton","cricket","chess"}
+union_1=my_games-friend_games
+print(union_1)

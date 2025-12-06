1e3075851098f8ec3a1b9d0fdf4b470550a969fc
amardeeplakkuntla <lakkuntlaamardeep@gmail.com>
2025-12-06 15:12:01 +0530

    loops programs

---
+++ loops_programs/loops3_program.py
@@ -0,0 +1,11 @@
+# sum of first 10 numbers
+
+sum = int(input("enter a number to sum: "))
+i = int(input("enter a number: "))
+
+while i <= 10:
+    sum = sum + i
+    i = i + 1
+
+print("Sum =", sum)
+print("i =",i)
